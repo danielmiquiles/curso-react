65ab2be4c382bc71f56816e7a65fe4aed9f59109
Daniel Miquiles <danielvictormiquiles@gmail.com>
2020-09-05 13:03:40 -0300

    inicio exercicio  calculadora

---
+++ calculadora/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Button(props) {
+  return (
+      <button className='Button'>0</button>
+  );
+}
+
+export default Button;
---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Calculator.css";
+import Button from "../components/Button";
+
+function Calculator(props) {
+  return <div className="Calculator">
+      <h2>
+          Calculator
+      </h2>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      
+  </div>
+}
+
+export default Calculator;
